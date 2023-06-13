6ba6fdeefa5260a5f1ea0e75198de4836c58924f
Kwang min <rhkdals195@naver.com>
2023-06-13 20:39:30 +0900

    Repository 테스트 환경 세팅 완료

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -1,5 +1,25 @@
 package site.metacoding.junitproject.domain;
 
-public interface BookRepositoryTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩
+public class BookRepositoryTest {
+
+    @Autowired // DI
+    private BookRepository bookRepository;
+
+    // 1. 책 등록
+    @Test
+    public void bookRegistration_test() {
+        System.out.println("책등록_test 실행");
+    }
+    // 2. 책 목록보기
+
+    // 3. 책 한건보기
+
+    // 4. 책 수정
+
+    // 5. 책 삭제
 }
