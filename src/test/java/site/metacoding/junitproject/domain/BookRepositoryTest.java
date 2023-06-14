6ae8157703d8c17e7c282b03ab2ff80e4cf76174
Kwang min <rhkdals195@naver.com>
2023-06-14 16:02:59 +0900

    책 목록 보기, 책 한 건 보기

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -2,6 +2,10 @@ package site.metacoding.junitproject.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,6 +15,21 @@ public class BookRepositoryTest {
 
     @Autowired // DI
     private BookRepository bookRepository;
+
+    // @BeforeAll // 테스트 시작전에 한번만 실행
+    @BeforeEach // 각 테스트 시작전에 한번씩 실행
+    public void dataReady() {
+        System.out.println("========================================================");
+        String title = "junit";
+        String author = "겟인데어";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
+        bookRepository.save(book);
+    } // 트랜잭션 종료 (저장된 데이터를 초기화함)
+      // 가정 1 : [ 데이터준비() + 1. 책등록 ] (T) , [ 데이터준비() + 2. 책목록보기] (T) -> 사이즈 1 (검증 완료)
+      // 가정 2 : [ 데이터준비() + 1. 책등록 + 데이터준비() + 2. 책목록보기 ] (T) -> 사이즈 2 (검증 실패)
 
     // 1. 책 등록
     @Test
@@ -29,10 +48,37 @@ public class BookRepositoryTest {
         // then (검증)
         assertEquals(title, bookPS.getTitle());
         assertEquals(author, bookPS.getAuthor());
-    }
+    } // 트랜잭션 종료 (저장된 데이터를 초기화함)
+
     // 2. 책 목록보기
+    @Test
+    public void bookList_test() {
+        // given
+        String title = "junit";
+        String author = "겟인데어";
+        // when
+        List<Book> booksPS = bookRepository.findAll();
+
+        System.out.println("사이즈 : ============================================     : " + booksPS.size());
+        // then
+        assertEquals(title, booksPS.get(0).getTitle());
+        assertEquals(author, booksPS.get(0).getAuthor());
+    } // 트랜잭션 종료 (저장된 데이터를 초기화함)
 
     // 3. 책 한건보기
+    @Test
+    public void bookOne_test() {
+        // given
+        String title = "junit";
+        String author = "겟인데어";
+
+        // when
+        Book bookPS = bookRepository.findById(1L).get();
+
+        // then
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+    } // 트랜잭션 종료 (저장된 데이터를 초기화함)
 
     // 4. 책 수정
 
