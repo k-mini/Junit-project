dcd91c98dc6a36a45366ad468a368846a44f3f8e
Kwang min <rhkdals195@naver.com>
2023-06-16 16:18:05 +0900

    북서비스 책등록 완료

--- src/main/java/site/metacoding/junitproject/web/dto/BookRespDto.java
+++ src/main/java/site/metacoding/junitproject/web/dto/BookRespDto.java
@@ -1,5 +1,22 @@
 package site.metacoding.junitproject.web.dto;
 
-public class BookRespDto {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import site.metacoding.junitproject.domain.Book;
 
+@NoArgsConstructor
+@Getter
+@Setter
+public class BookRespDto {
+    private Long id;
+    private String title;
+    private String author;
+
+    public BookRespDto toDto(Book bookPs){
+        this.id = bookPs.getId();
+        this.title = bookPs.getTitle();
+        this.author = bookPs.getAuthor();
+        return this;
+    }
 }
--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
